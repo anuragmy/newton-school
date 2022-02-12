3081f0a238e0cc0ff8acb9d9098e97832d06931d
anurag-felix <anurag.c@felixhealthcare.ai>
2022-02-12 19:39:39 +0530

    merge and quick sort added

--- test1.java
+++ test1.java
@@ -56,17 +56,93 @@ public class test1 {
 
     }
 
+    static int partition(int[] a, int start, int end) {
+        int pivot = a[end]; // last element // -> a[9] = 7
+        int i = start - 1; // 0-1 -> -1
+        // 11, 14, 26, 1, 2, 5, 4, 3, 8,
+        for (int j = start; j < end; j++) {
+            if (a[j] < pivot) { // j=8
+                i++; // i = 0, i=1, i=2, i=3, i=4,
+                // swap the elemnts
+                int temp = a[j]; // a[i] interchnge a[j] -> 1, 14, 26, 11, 2, 5, 4, 3, 8,
+                                 // 1, 2, 26, 11, 14, 5, 4, 3, 8,
+                                 // 1, 2, 5, 11, 14, 26, 4, 3, 8,
+                                 // 1, 2, 5, 4, 14, 26, 11, 3, 8,
+                                 // 1, 2, 5, 4, 3, 26, 11, 14, 8,
+                a[j] = a[i];
+                a[i] = temp;
+
+            }
+        }
+        // 1, 2, 5, 4, 3, 26, 11, 14, 8,
+        // i =4
+        i++;
+        int temp = a[i];
+        a[i] = pivot;
+        a[end] = temp;
+        // 1, 2, 5, 4, 3, 8, 11, 14, 26,
+        return i; // 5
+
+    }
+
+    static void qiuckSort(int[] a, int start, int end) {
+        if (start < end) {
+            int pivotIndex = partition(a, start, end);
+            qiuckSort(a, start, pivotIndex - 1); // 1,1
+            qiuckSort(a, pivotIndex + 1, end);// 5,9
+            // 1, 2, 5, 4, 3, ||| 8, 11, 14, 26,
+
+        }
+    }
+
+    static void merge(int[] a, int start, int mid, int end) {
+        int[] c = new int[end - start + 1];
+
+        int i = start;
+        int j = mid + 1;
+        int k = 0; //
+
+        while (i <= mid && j <= end) {
+            if (a[i] <= a[j]) {
+                c[k++] = a[i++];
+
+            } else {
+                c[k++] = a[j++];
+            }
+        }
+
+        while (i <= mid) {
+            c[k++] = a[i++];
+        }
+        while (j <= end) {
+            c[k++] = a[j++];
+        }
+        int len = c.length;
+        for (int l = 0, m = start; l < len; l++, m++) {
+            a[m] = c[l];
+        }
+
+    }
+
+    static void mergeSort(int[] a, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start) / 2;
+            mergeSort(a, start, mid);
+            mergeSort(a, mid + 1, end);
+            merge(a, start, mid, end);
+
+        }
+    }
+
     public static void main(String[] args) {
         int[] a = { 11, 14, 26, 1, 2, 5, 4, 3, 8, 7 };
-        // 1 2 6
+
         int len = a.length;
 
-        // bubble(a);
-        selectionSort(a);
-        for (int i = 0; i < len; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
+        // qiuckSort(a, 0, len - 1);
+        mergeSort(a, 0, len - 1);
+        printArr(a);
 
     }
 }
+
