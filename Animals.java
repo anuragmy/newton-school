1f61fb9ac0ba78374d53894b2ea3542f6748fea9
Anurag <anuragmy2729@gmail.com>
2022-02-24 00:34:33 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,33 @@
+//import java.lang.reflect.Constructor;
+
+public class Animals {
+
+    String name = "animal";
+    int age = 10;
+
+    Animals(String Name, int Age) {
+        this.name = Name;
+        this.age = Age;
+    }
+
+    public void speak() {
+        System.out.println(" it can speak");
+    }
+
+    public void setName(String Name) {
+        this.name = Name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void getName() {
+        System.out.println("Name of animal is:" + this.name);
+    }
+
+    public void getAge() {
+        System.out.println("Age of animal is:" + this.age);
+    }
+
+}
